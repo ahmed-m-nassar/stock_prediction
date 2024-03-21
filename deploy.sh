a301a7921255e591ca540855bfd0151dd8731ac2
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-03-21 14:22:20 +0300

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+source $HOME/miniconda/etc/profile.d/conda.sh
+
+# Create Conda environment from environment.yml
+conda env create -f environment.yml
+
+# Activate the Conda environment
+conda activate stock_predictor
--- unit_tests/data_ingestion/test_data_ingestion.py
+++ unit_tests/data_ingestion/test_data_ingestion.py
@@ -26,36 +26,3 @@ def test_download_stock_data():
     
     os.remove(output_path) 
        
-
-
-# @pytest.fixture
-# def sample_data():
-#     return pd.DataFrame({
-#         'Date': pd.date_range(start='2023-01-01', end='2023-01-10'),
-#         'Open': [100.0] * 10,
-#         'High': [105.0] * 10,
-#         'Low': [95.0] * 10,
-#         'Close': [102.0] * 10,
-#         'Volume': [1000000] * 10,
-#         'Adj Close': [101.0] * 10
-#     })
-
-
-# @patch('src.data_ingestion.data_ingestion.yf.download')
-# @patch('src.data_ingestion.data_ingestion.wandb.init')
-# @patch('src.data_ingestion.data_ingestion.wandb.save')
-# @patch('src.data_ingestion.data_ingestion.wandb.finish')
-# def test_get_stock_data_and_upload_to_wandb(mock_finish, mock_save, mock_init, mock_download, mock_args, sample_data):
-#     os.environ["WANDB_PROJECT"] = "stock_predictor"
-#     os.environ["WANDB_RUN_GROUP"] = "test"
-#     # Setting up the mock for yfinance.download
-#     mock_download.return_value = sample_data
-
-#     # Asserting calls to wandb functions
-#     mock_init.assert_called_once_with(name=mock_args.output_artifact, notes=mock_args.output_description)
-#     mock_save.assert_called_once_with(mock_args.output_artifact + '.csv')
-#     mock_finish.assert_called_once()
-
-#     # Asserting call to yfinance.download with correct arguments
-#     mock_download.assert_called_once_with(mock_args.stock_name, start=mock_args.start_date,
-#                                           end=mock_args.end_date)
