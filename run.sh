22dbeb04b0a8114adbdb487e258c5bc67d92439c
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-03-22 12:55:15 +0300

    update sh files

--- run.sh
+++ run.sh
@@ -16,6 +16,27 @@
 # search_miniconda
 # source $HOME/miniconda/bin/activate
 # eval "$(conda shell.bash hook)"
+# Check if Miniconda is already installed
+if [ ! -d "$HOME/miniconda" ]; then
+    # Install Miniconda
+    echo ""
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+fi;
+
+# Add Miniconda binaries to PATH
+# export PATH="$HOME/miniconda/bin:$PATH";
+echo PATH="\$HOME/miniconda/bin:\$PATH" >> ~/.bashrc
+source ~/.bashrc
+# Activate Conda environment
+source $HOME/miniconda/etc/profile.d/conda.sh;
+
+# Check if Conda environment exists, create if not
+if ! conda env list | grep -q "stock_predictor"; then
+    # Create Conda environment from environment.yml
+    echo ">>>>>>>>>>>>>>> HERE"
+    conda env create -f environment.yml
+fi;
 echo ">>>>>>>>>>>>>>> HERE"
 whoami
 conda activate stock_predictor
