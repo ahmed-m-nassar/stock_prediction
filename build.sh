5a268252fd6a64393c591e11f0fdebb2c36e5d48
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-03-22 10:50:15 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
+
+
+
+conda env create -f environment.yml
+source activate stock_predictor  # Replace with your environment name if different
+
 # Check if Miniconda is already installed
-if [ ! -d "$HOME/miniconda" ]; then
-    # Install Miniconda
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p $HOME/miniconda
-fi
-
-# Add Miniconda binaries to PATH
-export PATH="$HOME/miniconda/bin:$PATH"
-
-# Activate Conda environment
-source $HOME/miniconda/etc/profile.d/conda.sh
-
-conda env update -n base -f environment.yml --prune
-
-# Check if Conda environment exists, create if not
-# if ! conda env list | grep -q "stock_predictor"; then
-#     # Create Conda environment from environment.yml
-#     conda env create -f environment.yml
+# if [ ! -d "$HOME/miniconda" ]; then
+#     # Install Miniconda
+#     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+#     bash miniconda.sh -b -p $HOME/miniconda
 # fi
 
-# Activate the Conda environment
-# conda activate stock_predictor
+# # Add Miniconda binaries to PATH
+# export PATH="$HOME/miniconda/bin:$PATH"
 
-# # Verify Conda environment
-# conda info --envs
+# # Activate Conda environment
+# source $HOME/miniconda/etc/profile.d/conda.sh
+
+# conda env update -n base -f environment.yml --prune
+
+# # Check if Conda environment exists, create if not
+# # if ! conda env list | grep -q "stock_predictor"; then
+# #     # Create Conda environment from environment.yml
+# #     conda env create -f environment.yml
+# # fi
+
+# # Activate the Conda environment
+# # conda activate stock_predictor
+
+# # # Verify Conda environment
+# # conda info --envs
