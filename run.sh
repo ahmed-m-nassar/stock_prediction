51288856abddb57016611bec6baf2b4a7009e5d4
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-03-22 11:20:10 +0300

    update run

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source $HOME/miniconda/bin/activate
+# source $HOME/miniconda/bin/activate
+eval "$(conda shell.bash hook)"
 conda activate stock_predictor
 streamlit run dashboard.py
