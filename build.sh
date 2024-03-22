0810a864d3f461c56f5715fff94756f3c9ddda3d
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-03-22 11:29:58 +0300

    update run

--- build.sh
+++ build.sh
@@ -5,23 +5,22 @@ if [ ! -d "$HOME/miniconda" ]; then
     # Install Miniconda
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
     bash miniconda.sh -b -p $HOME/miniconda
-fi
+fi;
 
 # Add Miniconda binaries to PATH
-export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH";
 
 # Activate Conda environment
-source $HOME/miniconda/etc/profile.d/conda.sh
+source $HOME/miniconda/etc/profile.d/conda.sh;
 
-
-Check if Conda environment e xists, create if not
+# Check if Conda environment exists, create if not
 if ! conda env list | grep -q "stock_predictor"; then
     # Create Conda environment from environment.yml
     conda env create -f environment.yml
-fi
+fi;
 
-#Activate the Conda environment
-conda activate stock_predictor
+# Activate the Conda environment
+conda activate stock_predictor;
 
 # # Verify Conda environment
 # conda info --envs
